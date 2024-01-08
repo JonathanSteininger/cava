601fbc53267899bdf783366a86012765d3b05e94
Jonathan Steininger <jonathan.steininger@icloud.com>
2024-01-08 14:18:07 +1300

    miss-spelled. added documentation.

--- output/rawBars.c
+++ output/rawBars.c
@@ -21,34 +21,38 @@ int print_raw_bar_out(int bars_count, int fd, int is_binary, int bit_format, int
     ascii_range--;
 
     //buffer to write frame into
-    char mainBuf[barCharByteSize * height * bars_count + heigth + 1];
+    char mainBuf[barCharByteSize * height * bars_count + height + 1];
     int size_mainBuf = 0;
+    
+    //each line of bars
     for(int level = height -1; level >= 0; level--) {
+        //each bar
         for (int i = 0; i < bars_count; i++) {
             int f_ranged = f[i];
             if (f_ranged > ascii_range)
                 f_ranged = ascii_range;
 
-            //gets the index of bar
+            //gets the index for what bar to draw
             int index = f_ranged % barLayers;
-
             if ((level + 1) * barLayers <= f_ranged){
                 index = barLayers - 1;
             }else if (level * barLayers >= f_ranged){
                 index = barLayers;
             }
-
+            //appends char to buffer
             snprintf(mainBuf + size_mainBuf, barCharByteSize, barCharecters[index]);
             size_mainBuf += barCharByteOffset;
         }
+        //appends bar line delimiter
         if (level != 0){
             sprintf(mainBuf + size_mainBuf, "%c", layer_delim);
             size_mainBuf += 1;
         }
     }
+    //append end of framw delimiter
     sprintf(mainBuf + size_mainBuf, "%c", frame_delim);
     size_mainBuf += 1;
+    //writes to rawOutput path
     write(fd, &mainBuf, size_mainBuf);
-    
     return 0;
 }
